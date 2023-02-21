9ef10f8314e1c81be91fb1ea8455530a0f4b7926
null <975959057@qq.com>
2023-02-21 18:17:30 +0800

    feat: mod classify, character enum and translation

---
+++ src/views/mod/constant.ts
@@ -0,0 +1,89 @@
+export enum ModType {
+  character = 0,
+  NPC = 1,
+  enemy = 2,
+  weapon = 3,
+  entities = 4,
+  object = 5,
+  tcgCard = 6,
+  other = 10,
+}
+
+export enum CharacterEnum {
+  mona,
+  zhongli,
+  collei,
+  venti,
+  jean,
+  noelle,
+  albedo,
+  aloy,
+  amber,
+  itto,
+  barbara,
+  beidou,
+  bennett,
+  candace,
+  chongyun,
+  cyno,
+  diluc,
+  diona,
+  dori,
+  eula,
+  fischl,
+  ganyu,
+  gorou,
+  hutao,
+  kazuha,
+  kaeya,
+  ayaka,
+  ayato,
+  keqing,
+  klee,
+  kujousara,
+  shinobu,
+  lisa,
+  nilou,
+  ningguang,
+  qiqi,
+  raidenshogun,
+  razor,
+  rosaria,
+  kokomi,
+  sayu,
+  shenhe,
+  heizou,
+  sucrose,
+  tartaglia,
+  thoma,
+  tighnari,
+  traveler,
+  xiangling,
+  xiao,
+  xingqiu,
+  xinyan,
+  yae,
+  yanfei,
+  yelan,
+  yoimiya,
+  yunjin,
+  lumine,
+  layla,
+  nahida,
+  faruzan,
+  wanderer,
+  alhaitham,
+  yaoyao,
+}
+
+export enum NPCEnum {}
+
+export enum EnemyEnum {}
+
+export enum WeaponEnum {}
+
+export enum EntitiesEnum {}
+
+export enum ObjectEnum {}
+
+export enum TCGCardEnum {}
