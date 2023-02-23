3a7cf85aece0954b1a57e5f1a6e3f78b996566cb
null <975959057@qq.com>
2023-02-23 18:53:57 +0800

    fix: wrong path

--- build/plugins/i18n.ts
+++ build/plugins/i18n.ts
@@ -2,5 +2,5 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
 
 export default VueI18nPlugin({
-  include: path.resolve('./src/i18n/locales/**'),
+  include: path.resolve('./src/i18n/**'),
 })
